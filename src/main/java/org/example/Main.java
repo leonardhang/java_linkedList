e06276ef6a90537cef164235d6f83f490e3010dc
Leonardhang <Leonardhang@XA-CRM-LEONARD>
2025-04-09 10:56:45 +0800

    commit java demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
-
+        CustomList<String> customerList = new CustomList<>() {};
+        customerList.add("a");
+        customerList.add("b");
+        customerList.add("c");
+        customerList.add(0,"d");
+        customerList.removeLast();
+        customerList.sort(String::compareTo);
+        int target = 0;
+        while (target < customerList.size()) {
+            System.out.println(customerList.get(target));
+            target++;
+        }
     }
 }
